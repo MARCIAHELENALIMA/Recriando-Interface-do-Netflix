3236c4cd9ca92d46e899f3e448aeaa206db599da
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-01-05 20:15:59 -0300

    ok

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,4 @@
+const main = document.queryselector(".main")
 fetch( genres_list_htpp + new URLSearchParams({
     api_key: api_key
 }))
@@ -15,5 +16,26 @@ const fetchmovieslistbygenres = (id, genres)=>{
         page: Math.floor(math.random()*3) + 1
     }))
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+       makecategoryelent('${genres}_movies', data.results)
+    })
+    .catch(err => console.log(err))
+}
+
+const makecategoryelement = (category, data) =>{
+main.innerHTML +=
+
+<div class="movie-list">
+<button class="pre-btn">
+  <img src="img/prev.png" alt="previous button">
+</button>
+<h1 class="movie-category">${category}</h1>
+
+<div class="movie-container" id="${category}">
+
+</div>
+<button class="next-btn">
+<img src="img/next.png" alt="next button">
+</button>
+</div>
 }
