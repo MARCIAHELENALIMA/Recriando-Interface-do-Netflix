bfe4fc493c23194fee9c21a90780d66eee6acc31
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-01-05 21:48:27 -0300

    ok

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "9789ade8d8da37903a74370ff427b779"
+
+let img_url = "https://image.tmdb.org/t/p/w500" 
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
